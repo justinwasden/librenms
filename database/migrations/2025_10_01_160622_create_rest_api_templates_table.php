9feacc3e045f29c08ac6ebbd4622223ff9b8ef51
justinwasden <jwasden@gmail.com>
2025-10-02 10:57:34 -0500

    Update 2025_10_01_160622_create_rest_api_templates_table.php

--- database/migrations/2025_10_01_160622_create_rest_api_templates_table.php
+++ database/migrations/2025_10_01_160622_create_rest_api_templates_table.php
@@ -6,15 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+      public function up(): void
+    {
         Schema::create('rest_api_templates', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->string('vendor')->nullable();
+            // FIX: ADD THIS LINE:
             $table->text('description')->nullable();
             $table->text('template_data'); // JSON blob
             $table->timestamps();
         });
     }
+
 
     public function down(): void
     {
