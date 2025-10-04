b74854f5d9e7e13589639951ef46cfb31303161d
justinwasden <jwasden@gmail.com>
2025-10-04 11:51:39 -0500

    Update edit.blade.php

--- resources/views/settings/rest-api/templates/edit.blade.php
+++ resources/views/settings/rest-api/templates/edit.blade.php
@@ -12,31 +12,30 @@
 								    @csrf
 								    @method('PUT')
 								    @php
-    $endpoints = json_decode($template->template_data, true)['endpoints'] ?? [];
-@endphp
+    								$endpoints = json_decode($template->template_data, true)['endpoints'] ?? [];
+										@endphp
 
-@foreach ($endpoints as $index => $endpoint)
-    <div class="card mb-3">
-        <div class="card-header bg-light">
-            <strong>Endpoint {{ $index + 1 }}: {{ $endpoint['name'] ?? 'Unnamed' }}</strong>
-        </div>
-        <div class="card-body">
-            <div class="form-group">
-                <label>Path</label>
-                <input type="text" class="form-control" name="template_data[endpoints][{{ $index }}][path]" value="{{ $endpoint['path'] ?? '' }}">
-            </div>
-            <div class="form-group">
-                <label>Method</label>
-                <input type="text" class="form-control" name="template_data[endpoints][{{ $index }}][method]" value="{{ $endpoint['method'] ?? '' }}">
-            </div>
-            <div class="form-group">
-                <label>Parameters (JSON)</label>
-                <textarea class="form-control" name="template_data[endpoints][{{ $index }}][params]" rows="3">{{ json_encode($endpoint['params'] ?? [], JSON_PRETTY_PRINT) }}</textarea>
-            </div>
-        </div>
-    </div>
-@endforeach
-								    @include('settings.rest-api.templates._form')
+										@foreach ($endpoints as $index => $endpoint)
+										    <div class="card mb-3">
+										        <div class="card-header bg-light">
+										            <strong>Endpoint {{ $index + 1 }}: {{ $endpoint['name'] ?? 'Unnamed' }}</strong>
+										        </div>
+										        <div class="card-body">
+										            <div class="form-group">
+										                <label>Path</label>
+										                <input type="text" class="form-control" name="template_data[endpoints][{{ $index }}][path]" value="{{ $endpoint['path'] ?? '' }}">
+										            </div>
+										            <div class="form-group">
+										                <label>Method</label>
+										                <input type="text" class="form-control" name="template_data[endpoints][{{ $index }}][method]" value="{{ $endpoint['method'] ?? '' }}">
+										            </div>
+										            <div class="form-group">
+										                <label>Parameters (JSON)</label>
+										                <textarea class="form-control" name="template_data[endpoints][{{ $index }}][params]" rows="3">{{ json_encode($endpoint['params'] ?? [], JSON_PRETTY_PRINT) }}</textarea>
+										            </div>
+										        </div>
+										    </div>
+										@endforeach
 								    <button type="submit" class="btn btn-primary">Update</button>
 								</form>
             </div>
