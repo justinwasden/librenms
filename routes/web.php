32b9f9cc800dacf0ce8baa0888ea2397ceaa4e95
justinwasden <jwasden@gmail.com>
2025-10-06 22:19:06 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -156,8 +156,8 @@ Route::middleware(['auth'])->group(function () {
         Route::prefix('settings')->name('settings.')->group(function () {
             Route::resource('metric-field-mappings', \App\Http\Controllers\Settings\MetricFieldMappingController::class)
                 ->parameters(['metric-field-mappings' => 'mapping']);
-            Route::post('metric-field-mappings/import', [\App\Http\Controllers\Settings\MetricFieldMappingController::class, 'importFromJson'])->name('metric-field-mappings.import');
-            Route::get('metric-field-mappings/export', [\App\Http\Controllers\Settings\MetricFieldMappingController::class, 'exportToJson'])->name('metric-field-mappings.export');
+				    Route::post('metric-field-mappings/import', [\App\Http\Controllers\Settings\MetricFieldMappingController::class, 'importFromJson'])->name('.metric-field-mappings.import');
+				    Route::get('metric-field-mappings/export', [\App\Http\Controllers\Settings\MetricFieldMappingController::class, 'exportToJson'])->name('.metric-field-mappings.export');
 
             // Existing routes continue below:
             Route::post('metric-field-mappings/{mapping}/toggle', [\App\Http\Controllers\Settings\MetricFieldMappingController::class, 'toggle'])->name('metric-field-mappings.toggle');
