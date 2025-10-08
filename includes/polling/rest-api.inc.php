8e1046d9c4e141d6da4196f63dd842a820c4aaeb
justinwasden <jwasden@gmail.com>
2025-10-08 08:37:11 -0500

    Update rest-api.inc.php

--- includes/polling/rest-api.inc.php
+++ includes/polling/rest-api.inc.php
@@ -1,4 +1,14 @@
 <?php
+
+// Bootstrap Laravel to use Eloquent models and facades
+if (!defined('LARAVEL_START')) {
+    require base_path('bootstrap/autoload.php');
+    $app = require_once base_path('bootstrap/app.php');
+    $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+    $kernel->bootstrap();
+}
+
+use App\Models\Device;
 use App\Pollers\RestApiPoller;
 
 if (!isset($device) || empty($device['device_id'])) {
