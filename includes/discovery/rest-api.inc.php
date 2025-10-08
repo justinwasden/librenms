7b4bf189bfe74ed7a61c18a1130b7662cf98d690
justinwasden <jwasden@gmail.com>
2025-10-08 10:53:40 -0500

    Update rest-api.inc.php

--- includes/discovery/rest-api.inc.php
+++ includes/discovery/rest-api.inc.php
@@ -1,24 +1,14 @@
 <?php
 
-/**
- * rest-api.inc.php
- * LibreNMS REST API Discovery Bridge
- */
-
-use Illuminate\Support\Facades\Log;
+use LibreNMS\Interfaces\Discovery;
 
 if (!defined('LibreNMS')) {
     exit;
 }
 
 try {
-    if (!class_exists(\App\Discovery\ApiDiscoveryBridge::class)) {
-        require_once base_path('app/Discovery/ApiDiscoveryBridge.php');
-    }
-
-    $bridge = new \App\Discovery\ApiDiscoveryBridge($device);
-    $bridge->run();
-
-} catch (Throwable $e) {
-    Log::error("[REST-API Discovery Bridge] {$device['hostname']}: {$e->getMessage()}");
+    $runner = new RestApiDiscovery($device);
+    $runner->handle();
+} catch (Exception $e) {
+    \Log::error("REST API Discovery failed for device {$device['hostname']}: {$e->getMessage()}");
 }
