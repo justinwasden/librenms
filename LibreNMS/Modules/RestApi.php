36ae643b798221fde35faefab08af1c649800ff5
justinwasden <jwasden@gmail.com>
2025-10-22 00:24:46 -0500

    Update RestApi.php

--- LibreNMS/Modules/RestApi.php
+++ LibreNMS/Modules/RestApi.php
@@ -19,67 +19,67 @@ class RestApi implements Module
     }
 
     public function poll(OS $os, DataStorageInterface $datastore): void
-{
-    $device = $os->getDevice();
-    if ($this->deviceHasApiConnections($device->id)) {
-        $this->pollerService->pollDeviceById($device->id);
-    }
-}
+		{
+		    $device = $os->getDevice();
+		    if ($this->deviceHasApiConnections($device->id)) {
+		        $this->pollerService->pollDeviceById($device->id);
+		    }
+		}
 
-public function discover(OS $os): void
-{
-    $device = $os->getDevice();
-    if ($this->deviceHasApiConnections($device->id)) {
-        $this->pollerService->discoverDeviceById($device->id);
-    }
-}
+		public function discover(OS $os): void
+		{
+		    $device = $os->getDevice();
+		    if ($this->deviceHasApiConnections($device->id)) {
+		        $this->pollerService->discoverDeviceById($device->id);
+		    }
+		}
 
-    public function shouldPoll(OS $os, ModuleStatus $status): bool
-{
-    $device = $os->getDevice();
-    return $this->deviceHasApiConnections($device->id);
-}
+		public function shouldPoll(OS $os, ModuleStatus $status): bool
+		{
+		    $device = $os->getDevice();
+		    return $this->deviceHasApiConnections($device->id);
+		}
 
-public function shouldDiscover(OS $os, ModuleStatus $status): bool
-{
-    $device = $os->getDevice();
-    return $this->deviceHasApiConnections($device->id);
-}
+		public function shouldDiscover(OS $os, ModuleStatus $status): bool
+		{
+		    $device = $os->getDevice();
+		    return $this->deviceHasApiConnections($device->id);
+		}
 
     public function dataExists(Device $device): bool
-{
-    return $this->deviceHasApiConnections($device->id);
-}
+		{
+		    return $this->deviceHasApiConnections($device->id);
+		}
 
     public function cleanup(Device $device): int
-{
-    $count = 0;
+		{
+		    $count = 0;
 
-    // Delete all REST API connections and cascade deletes to endpoints/metrics
-    $connections = $device->restApiConnections();
-    $count += $connections->count();
-    $connections->delete();
+		    // Delete all REST API connections and cascade deletes to endpoints/metrics
+		    $connections = $device->restApiConnections();
+		    $count += $connections->count();
+		    $connections->delete();
 
-    return $count;
-}
-public function dump(Device $device, string $type): ?array
-{
-    return [
-        'rest_api_connections' => $device->restApiConnections()
-            ->with(['credential', 'endpoints'])
-            ->get()
-            ->map(function ($conn) {
-                return [
-                    'name' => $conn->name,
-                    'base_url' => $conn->base_url,
-                    'enabled' => $conn->enabled,
-                    'credential_type' => $conn->credential?->authenticationType?->name,
-                    'endpoints_count' => $conn->endpoints->count(),
-                ];
-            })
-            ->toArray(),
-    ];
-}
+		    return $count;
+		}
+		public function dump(Device $device, string $type): ?array
+		{
+		    return [
+		        'rest_api_connections' => $device->restApiConnections()
+		            ->with(['credential', 'endpoints'])
+		            ->get()
+		            ->map(function ($conn) {
+		                return [
+		                    'name' => $conn->name,
+		                    'base_url' => $conn->base_url,
+		                    'enabled' => $conn->enabled,
+		                    'credential_type' => $conn->credential?->authenticationType?->name,
+		                    'endpoints_count' => $conn->endpoints->count(),
+		                ];
+		            })
+		            ->toArray(),
+		    ];
+		}
     public function dependencies(): array
     {
         return [];
