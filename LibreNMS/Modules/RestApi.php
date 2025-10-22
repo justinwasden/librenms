7b461b578eb4295f88a2199ca8541a218ed2c5ba
justinwasden <jwasden@gmail.com>
2025-10-22 00:07:58 -0500

    Update RestApi.php

--- LibreNMS/Modules/RestApi.php
+++ LibreNMS/Modules/RestApi.php
@@ -1,21 +1,16 @@
 <?php
-/**
- * File: /opt/librenms/LibreNMS/Modules/RestApi.php
- * Purpose: Legacy RestApi poller module updated to use RestApiPollerService
- */
-
 namespace LibreNMS\Modules;
 
 use App\Services\RestApi\RestApiPollerService;
 use LibreNMS\Devices\Device;
+use LibreNMS\Interfaces\Module as ModuleInterface;
 
-class RestApi
+class RestApi implements ModuleInterface
 {
     protected $pollerService;
 
     public function __construct()
     {
-        // Use Laravel service container to instantiate the new poller service
         $this->pollerService = app(RestApiPollerService::class);
     }
 
@@ -23,38 +18,18 @@ class RestApi
      * Poll a device if it has REST API connections configured.
      *
      * @param Device $device
-     * @return void
      */
-    public function pollDevice(Device $device)
+    public function poll(Device $device): void
     {
-        // Check if this device has API connections enabled
         if ($this->deviceHasApiConnections($device->id)) {
             $this->pollerService->pollDevice($device);
         }
     }
 
-    /**
-     * Determine if a device has REST API connections configured
-     *
-     * @param int $deviceId
-     * @return bool
-     */
     protected function deviceHasApiConnections(int $deviceId): bool
     {
-        // Assumes you have a table `rest_api_connections` with device_id column
         return \DB::table('rest_api_connections')
             ->where('device_id', $deviceId)
             ->exists();
-    }
-
-    /**
-     * Legacy hook for poller modules (called by native LibreNMS poller)
-     *
-     * @param Device $device
-     * @return void
-     */
-    public function poll(Device $device)
-    {
-        $this->pollDevice($device);
     }
 }
