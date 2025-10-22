fb9c19a132df7852a3a2eb515d7dd6eb29f03732
justinwasden <jwasden@gmail.com>
2025-10-22 00:11:27 -0500

    Update RestApi.php

--- LibreNMS/Modules/RestApi.php
+++ LibreNMS/Modules/RestApi.php
@@ -5,19 +5,17 @@ use App\Services\RestApi\RestApiPollerService;
 use LibreNMS\Interfaces\Module as ModuleInterface;
 use LibreNMS\OS;
 use LibreNMS\Interfaces\Data\DataStorageInterface;
+use LibreNMS\Polling\ModuleStatus;
 
 class RestApi implements ModuleInterface
 {
-    protected RestApiPollerService $pollerService;
+     protected RestApiPollerService $pollerService;
 
     public function __construct()
     {
         $this->pollerService = app(RestApiPollerService::class);
     }
 
-    /**
-     * Called by the poller
-     */
     public function poll(OS $os, DataStorageInterface $datastore): void
     {
         $deviceId = $os->device_id;
@@ -27,57 +25,37 @@ class RestApi implements ModuleInterface
         }
     }
 
-    /**
-     * Called by discovery engine to determine if polling should happen
-     */
-    public function shouldPoll(OS $os): bool
+    // Updated signatures with ModuleStatus
+    public function shouldPoll(OS $os, ModuleStatus $status): bool
     {
         return $this->deviceHasApiConnections($os->device_id);
     }
 
-    /**
-     * Called by discovery engine to determine if discovery should happen
-     */
-    public function shouldDiscover(OS $os): bool
+    public function shouldDiscover(OS $os, ModuleStatus $status): bool
     {
         return $this->deviceHasApiConnections($os->device_id);
     }
 
-    /**
-     * List any module dependencies
-     */
     public function dependencies(): array
     {
         return [];
     }
 
-    /**
-     * Module name
-     */
     public function getName(): string
     {
         return 'REST API';
     }
 
-    /**
-     * Module version
-     */
     public function getVersion(): string
     {
         return '1.0';
     }
 
-    /**
-     * Module author
-     */
     public function getAuthor(): string
     {
         return 'JDub';
     }
 
-    /**
-     * Check if a device has REST API connections
-     */
     protected function deviceHasApiConnections(int $deviceId): bool
     {
         return \DB::table('rest_api_connections')
