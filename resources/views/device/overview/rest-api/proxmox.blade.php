56f39865c81bd91301c35a430d540f2f54060733
justinwasden <jwasden@gmail.com>
2025-10-24 10:46:03 -0500

    Update proxmox.blade.php

--- resources/views/device/overview/rest-api/proxmox.blade.php
+++ resources/views/device/overview/rest-api/proxmox.blade.php
@@ -4,9 +4,8 @@
 @php
 use Illuminate\Support\Facades\DB;
 use LibreNMS\Util\Number;
-use LibreNMS\Util\Color;
-
-// --- Data Fetch ---
+use App\Models\Storage;
+use App\Models\EntPhysical;
 
 $device_id = $device['device_id'];
 
@@ -47,55 +46,51 @@ $has_metrics = $mem_data || $cpu_data || $storage->count() > 0;
 
 @endphp
 
-@if(!$has_metrics)
+@if(!$has_data)
 <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-12">
         <div class="alert alert-info">
             <i class="fa fa-info-circle"></i>
             <strong>No REST API metrics found for Proxmox.</strong>
-            <br>Ensure endpoints are configured and polled successfully.
+            <br>Data will appear after the next polling cycle.
         </div>
     </div>
 </div>
 @else
 
 <div class="row">
-    <div class="col-md-4">
-        <div class="panel panel-default panel-condensed">
-            <div class="panel-heading">
+    <div class="col-md-6">
+        <x-panel class="device-overview panel-condensed">
+            <x-slot name="heading">
                 <i class="fa fa-server fa-lg icon-theme"></i> <strong>Proxmox Node Overview: {{ $device['sysName'] }}</strong>
-            </div>
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-md-4">
-                        <table class="table table-condensed table-striped">
-                            <tr><th>Node Name</th><td>{{ $device['sysName'] }}</td></tr>
-                            <tr><th>Hostname</th><td>{{ $device['hostname'] }}</td></tr>
-                            <tr><th>Cluster Status</th>
-                                <td>
-                                    @php
-                                        // Assuming 1 = Quorate/Online, 0 = Offline
-                                        $status_value = $cluster_status->sensor_current ?? 'N/A';
-                                        $status_text = ($status_value == 1) ? 'ONLINE' : (($status_value === 0) ? 'OFFLINE' : 'N/A');
-                                        $label = ($status_value == 1) ? 'success' : 'danger';
-                                    @endphp
-                                    <span class="label label-{{ $label }}">{{ $status_text }}</span>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-
-                    <div class="col-md-4">
-                        <h4>CPU Utilization</h4>
-                        {!! print_percentage_bar(350, 40, $cpu_util, number_format($cpu_util, 1) . "%", 'ffffff', $cpu_bg['left'], 100 - $cpu_util, 'ffffff', $cpu_bg['right']) !!}
+            </x-slot>
+            <table class="table table-hover table-condensed table-striped tw:mb-0!">
+                <tbody>
+                    <tr><th>Node Name</th><td>{{ $device['sysName'] }}</td></tr>
+                    <tr><th>Hostname</th><td>{{ $device['hostname'] }}</td></tr>
+                    <tr><th>Cluster Status</th>
+		                    <td>
+		                                    @php
+		                                        // Assuming 1 = Quorate/Online, 0 = Offline
+		                                        $status_value = $cluster_status->sensor_current ?? 'N/A';
+		                                        $status_text = ($status_value == 1) ? 'ONLINE' : (($status_value === 0) ? 'OFFLINE' : 'N/A');
+		                                        $label = ($status_value == 1) ? 'success' : 'danger';
+		                                    @endphp
+		                                    <span class="label label-{{ $label }}">{{ $status_text }}</span>
+		                   </td>
+                   </tr>
+                   <tr>
+                        <th style="font-weight: bold;">CPU Utilization</th>
+                        <td>{!! print_percentage_bar(350, 40, $cpu_util, number_format($cpu_util, 1) . "%", 'ffffff', $cpu_bg['left'], 100 - $cpu_util, 'ffffff', $cpu_bg['right']) !!}
                         <p class="text-muted small text-center mt-2">
                             {{ number_format($cpu_util, 1) }}% Max/Avg Usage
                         </p>
-                    </div>
-
-                    <div class="col-md-4">
-                        <h4>Physical Memory Usage</h4>
-                        @if($total_mem > 0)
+<												/td>
+                 </tr>
+								<tr>
+								<th>Physical Memory Usage</th>
+								<td>
+										   @if($total_mem > 0)
                             {!! print_percentage_bar(350, 40, $mem_percent, \LibreNMS\Util\Number::formatBi($used_mem) . " / " . \LibreNMS\Util\Number::formatBi($total_mem), 'ffffff', $mem_bg['left'], $total_mem - $used_mem, 'ffffff', $mem_bg['right']) !!}
                             <p class="text-muted small text-center mt-2">
                                 {{ number_format($mem_percent, 2) }}% Utilization
@@ -103,12 +98,13 @@ $has_metrics = $mem_data || $cpu_data || $storage->count() > 0;
                         @else
                             <p class="text-muted text-center">Memory data not available in mempools table.</p>
                         @endif
+
+								</td>
+
+								</tr>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 @if($storage->count() > 0)
 <div class="row">
