4ba2300c8e1e9812349a228b26899a047c266783
justinwasden <jwasden@gmail.com>
2025-10-25 12:35:23 -0500

    Update DeviceApiSettings.php

--- LibreNMS/Util/DeviceApiSettings.php
+++ LibreNMS/Util/DeviceApiSettings.php
@@ -8,7 +8,7 @@ class DeviceApiSettings
 {
     public static function restEnabled(Device $device): bool
     {
-        return (bool) ($device->attribs['rest_enabled'] ?? 0);
+        return (bool) (($device->attribs['rest_enabled'] ?? 0));
     }
 
     public static function vendor(Device $device): ?string
@@ -19,46 +19,68 @@ class DeviceApiSettings
     public static function httpOptions(Device $device): array
     {
         $a = $device->attribs ?? [];
-        return [
-            'base_url'   => rtrim($a['rest_base_url'] ?? $a['proxmox_base_url'] ?? '', '/'),
-            'verify_tls' => (bool)($a['rest_verify_tls'] ?? $a['proxmox_verify_tls'] ?? true),
-            'timeout_ms' => (int)($a['rest_timeout_ms'] ?? $a['proxmox_timeout_ms'] ?? 5000),
-            'proxy'      => $a['rest_proxy'] ?? $a['proxmox_proxy'] ?? null,
-            'headers'    => !empty($a['rest_headers']) ? (json_decode($a['rest_headers'], true) ?: []) : [],
-        };
+
+        $headers = array();
+        if (!empty($a['rest_headers'])) {
+            $decoded = json_decode($a['rest_headers'], true);
+            if (is_array($decoded)) {
+                $headers = $decoded;
+            }
+        }
+
+        return array(
+            'base_url'   => rtrim(($a['rest_base_url'] ?? ($a['proxmox_base_url'] ?? '')), '/'),
+            'verify_tls' => (bool)(($a['rest_verify_tls'] ?? ($a['proxmox_verify_tls'] ?? true))),
+            'timeout_ms' => (int)(($a['rest_timeout_ms'] ?? ($a['proxmox_timeout_ms'] ?? 5000))),
+            'proxy'      => $a['rest_proxy'] ?? ($a['proxmox_proxy'] ?? null),
+            'headers'    => $headers,
+        );
     }
 
     // Pure Storage specific
     public static function pureOptions(Device $device): array
     {
-        $a = $device->attribs ?? [];
-        $token = !empty($a['rest_token_enc']) ? Crypt::decryptString($a['rest_token_enc']) : ($a['rest_token'] ?? '');
-        return [
+        $a = $device->attribs ?? array();
+        $token = '';
+        if (!empty($a['rest_token_enc'])) {
+            $token = Crypt::decryptString($a['rest_token_enc']);
+        } elseif (!empty($a['rest_token'])) {
+            $token = $a['rest_token'];
+        }
+
+        return array(
             'auth_type' => $a['rest_auth_type'] ?? 'apikey',
             'token'     => $token,
-        ];
+        );
     }
 
     // Proxmox specific
     public static function proxmoxOptions(Device $device): array
     {
-        $a = $device->attribs ?? [];
+        $a = $device->attribs ?? array();
         $mode = $a['proxmox_auth_type'] ?? 'token';
-        $opts = ['auth_type' => $mode];
+
+        $opts = array('auth_type' => $mode);
 
         if ($mode === 'token') {
-            $secret = !empty($a['proxmox_token_enc']) ? Crypt::decryptString($a['proxmox_token_enc']) : '';
-            $opts += [
+            $secret = '';
+            if (!empty($a['proxmox_token_enc'])) {
+                $secret = Crypt::decryptString($a['proxmox_token_enc']);
+            }
+            $opts = array_merge($opts, array(
                 'token_user' => $a['proxmox_token_user'] ?? '',
                 'token_id'   => $a['proxmox_token_id'] ?? '',
                 'token'      => $secret,
-            ];
+            ));
         } else {
-            $password = !empty($a['proxmox_password_enc']) ? Crypt::decryptString($a['proxmox_password_enc']) : '';
-            $opts += [
+            $password = '';
+            if (!empty($a['proxmox_password_enc'])) {
+                $password = Crypt::decryptString($a['proxmox_password_enc']);
+            }
+            $opts = array_merge($opts, array(
                 'username' => $a['proxmox_username'] ?? '',
                 'password' => $password,
-            ];
+            ));
         }
 
         return $opts;
