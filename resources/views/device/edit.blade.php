c73b36e1bf51186c6ae9aac9d54623205ef90105
justinwasden <jwasden@gmail.com>
2025-10-25 00:21:04 -0500

    Update edit.blade.php

--- resources/views/device/edit.blade.php
+++ resources/views/device/edit.blade.php
@@ -108,7 +108,8 @@
 
                 <div class="mt-3">
                     <button type="submit" class="btn btn-primary">Save Changes</button>
-                    <a href="{{ route('device.show', ['device' => $device_id]) }}" class="btn btn-secondary">Cancel</a>
+                    {{-- Cancel goes back to the legacy device page --}}
+                    <a href="{{ url("device/{$device_id}") }}" class="btn btn-secondary">Cancel</a>
                 </div>
             </form>
         @else
