ce9d5e6357041444f4331911e847bfe841efb8e3
justinwasden <jwasden@gmail.com>
2025-10-27 23:16:30 -0500

    repairs

--- includes/polling/sensors/percent/enexus.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Arr;
-
-if ($sensor['sensor_type'] === 'batteryTestQuality') {
-    $raw = SnmpQuery::walk($sensor['sensor_oid'])->values();
-    $sensor_value = Arr::last($raw);
-    unset($raw);
-}
